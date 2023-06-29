2c02f18b077547c54084f26e5b8851124466d61d
holomdev <holom.dev@gmail.com>
2023-06-29 16:36:44 -0600

    feat(authentication): implement signIn sign Up

--- src/iam/authentication/authentication.service.ts
+++ src/iam/authentication/authentication.service.ts
@@ -1,9 +1,14 @@
-import { Injectable, ConflictException } from '@nestjs/common';
+import {
+  Injectable,
+  ConflictException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../../users/entities/user.entity';
 import { Repository } from 'typeorm';
 import { HashingService } from '../hashing/hashing.service';
 import { SignUpDto } from './dto/sign-up.dto';
+import { SignInDto } from './dto/sign-in.dto';
 
 @Injectable()
 export class AuthenticationService {
@@ -27,5 +32,26 @@ export class AuthenticationService {
       }
       throw err;
     }
+  }
+
+  async signIn(signInDto: SignInDto) {
+    const user = await this.userRepository.findOneBy({
+      email: signInDto.email,
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('User does not exists');
+    }
+
+    const isEqual = await this.hashingService.compare(
+      signInDto.password,
+      user.password,
+    );
+
+    if (!isEqual) {
+      throw new UnauthorizedException('Password does not match');
+    }
+
+    return true;
   }
 }
