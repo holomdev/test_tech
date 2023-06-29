1ee294ea2a21435d78ad1ad9e1966ad76060f67e
holomdev <holom.dev@gmail.com>
2023-06-29 15:52:18 -0600

    feat(authentication): add signup method to service

--- src/iam/authentication/authentication.service.ts
+++ src/iam/authentication/authentication.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../../users/entities/user.entity';
+import { Repository } from 'typeorm';
+import { HashingService } from '../hashing/hashing.service';
+import { SignUpDto } from './dto/sign-up.dto';
 
 @Injectable()
-export class AuthenticationService {}
+export class AuthenticationService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private readonly hashingService: HashingService,
+  ) {}
+
+  async signUp(signUpDto: SignUpDto) {
+    try {
+      const user = new User();
+      user.email = signUpDto.email;
+      user.name = signUpDto.name;
+      user.username = signUpDto.username;
+      user.password = await this.hashingService.hash(signUpDto.password);
+
+      await this.userRepository.save(user);
+    } catch (err) {
+      if (err.code === '23505') {
+        throw new ConflictException();
+      }
+      throw err;
+    }
+  }
+}
