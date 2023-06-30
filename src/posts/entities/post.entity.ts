89736d7ed23764cad0d6ddc20ddf326578e4a575
holomdev <holom.dev@gmail.com>
2023-06-30 10:27:51 -0600

    feat(user): relation one to many post

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Post {
@@ -10,4 +11,7 @@ export class Post {
 
   @Column({ type: 'text' })
   body: string;
+
+  @ManyToOne(() => User, (user) => user.posts)
+  user: User;
 }
