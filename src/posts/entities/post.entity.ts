707a253cad87e47a5f67f4d26e15a403f39280bd
holomdev <holom.dev@gmail.com>
2023-07-02 23:44:44 -0600

    fix: fix entities paths

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,5 +1,5 @@
-import { User } from 'src/users/entities/user.entity';
 import { Comment } from '../../comments/entities/comment.entity';
+import { User } from '../../users/entities/user.entity';
 import {
   Column,
   Entity,
