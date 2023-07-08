974813e860d6cbebb9eb26323b35c8a83b2344bc
holomdev <holom.dev@gmail.com>
2023-07-08 10:12:14 -0600

    test(authenticationController): should be defined

---
+++ src/iam/authentication/authentication.controller.spec.ts
@@ -0,0 +1,28 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthenticationController } from './authentication.controller';
+import { AuthenticationService } from './authentication.service';
+
+describe('AuthenticationController', () => {
+  let controller: AuthenticationController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AuthenticationController],
+      providers: [
+        {
+          provide: AuthenticationService,
+          useValue: {
+            signUp: jest.fn(),
+            signIn: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    controller = module.get<AuthenticationController>(AuthenticationController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
