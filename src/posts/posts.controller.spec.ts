be0afcc4a3d3c03a15f807e4823d75fed72ef9e2
holomdev <holom.dev@gmail.com>
2023-07-09 11:11:22 -0600

    test(postsController): should be defined

---
+++ src/posts/posts.controller.spec.ts
@@ -0,0 +1,35 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PostsController } from './posts.controller';
+import { PostsService } from './posts.service';
+
+describe('PostsController', () => {
+  let controller: PostsController;
+  let postsService: PostsService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [PostsController],
+      providers: [
+        {
+          provide: PostsService,
+          useValue: {
+            create: jest.fn(),
+            createComment: jest.fn(),
+            findAll: jest.fn(),
+            findAllComments: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    controller = module.get<PostsController>(PostsController);
+    postsService = module.get<PostsService>(PostsService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
