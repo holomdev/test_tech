dc49f2596bc29ce9269829e731b0be1f59a62bff
holomdev <holom.dev@gmail.com>
2023-07-09 16:41:02 -0600

    feat(authentication): add e2e test biolerplate

---
+++ test/authentication/authentication.e2e-spec.ts
@@ -0,0 +1,33 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../../src/app.module';
+
+describe('[Feature] Authentication - /authentication (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        whitelist: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    );
+    await app.init();
+  });
+
+  it.todo('sign-up [GET /]');
+  it.todo('sign-in [GET /]');
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
