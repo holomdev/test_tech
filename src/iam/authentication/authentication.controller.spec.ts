93195055532ba77331aecca4414012daf2f80dc7
holomdev <holom.dev@gmail.com>
2023-07-09 10:07:05 -0600

    test(authenticationController): should call method signUp in authService

--- src/iam/authentication/authentication.controller.spec.ts
+++ src/iam/authentication/authentication.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
 import { SignInDto } from './dto/sign-in.dto';
+import { SignUpDto } from './dto/sign-up.dto';
 
 describe('AuthenticationController', () => {
   let controller: AuthenticationController;
@@ -42,5 +43,20 @@ describe('AuthenticationController', () => {
     await controller.signIn(signInDto);
 
     expect(authenticationService.signIn).toHaveBeenCalledWith(signInDto);
+  });
+
+  it('should call method signUp in authService', async () => {
+    const signUpDto: SignUpDto = {
+      email: 'test@example.com',
+      password: 'password123',
+      username: 'username',
+      name: 'name',
+    };
+
+    jest.spyOn(authenticationService, 'signUp');
+
+    await controller.signUp(signUpDto);
+
+    expect(authenticationService.signUp).toHaveBeenCalledWith(signUpDto);
   });
 });
